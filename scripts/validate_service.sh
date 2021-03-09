b16cb0f766ec8626bdb85b585b4216ba61fa1309
Naila Nur <23naila.n@gmail.com>
2021-03-09 17:08:55 -0500

    .

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,4 @@
-status=$(curl -s -o /dev/null -I -w "%{http_code}" http://ec2-3-141-29-82.us-east-2.compute.amazonaws.com:8000)
+status=$(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:8000)
 echo "http status:  $status"
 if [ $status = "200" ] ; then
     exit 0
