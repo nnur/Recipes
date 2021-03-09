1611bd4915390d6c44f6f82e9a20e9169c15557e
Naila Nur <23naila.n@gmail.com>
2021-03-09 15:34:49 -0500

    added health check

---
+++ scripts/validate_service.sh
@@ -0,0 +1,8 @@
+status=$(curl -s -o /dev/null -I -w "%{http_code}" http://ec2-3-141-29-82.us-east-2.compute.amazonaws.com)
+if [ $status = "200" ] ; then
+    echo $status
+    exit 0
+else
+    echo $status
+    exit 1
+fi
