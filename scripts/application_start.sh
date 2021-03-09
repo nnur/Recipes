50946038f365acfe8a0c1213ad239e41824a3578
Naila Nur <23naila.n@gmail.com>
2021-03-09 18:09:02 -0500

    .

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sh /etc/profile.d/recipe.sh
 cd /usr/apps/recipes
 . .venv/bin/activate
 gunicorn --bind 0.0.0.0:8000 -w 4 recipes.main:app --error-logfile "/var/log/gunicorn-logs.txt" --log-level "info" --daemon
--- scripts/set_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export FLASK_APP=recipes.main
-export DATABASE_URL="postgresql://postgres:tslzXGUXh1iY1WlxbxBO@recipe-db-instance.cmzxnjhlalty.us-east-2.rds.amazonaws.com:5432/postgres"
-export SQLALCHEMY_TRACK_MODIFICATIONS=False
-export ENV=production
-export FLASK_ENV=production
