dcf5cd187ca0aa446b08877f4e3770b43c33e4b5
Naila Nur <23naila.n@gmail.com>
2021-03-10 19:00:51 -0500

    .

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 . /etc/profile.d/recipe.sh
 cd /usr/apps/recipes
 . .venv/bin/activate
-flask db migrate
+flask db upgrade
 gunicorn --bind 0.0.0.0:8000 -w 4 recipes.main:app --error-logfile "/var/log/gunicorn-logs.txt" --log-level "info" --daemon
